037d0b172def5bf19378b49b193ef81b65bbf8ab
Osarugue Enehizena <nehirugue@gmail.com>
2024-11-27 10:18:04 +0100

    done with index.tsx

--- Week-10/offline-first-app/app/index.tsx
+++ Week-10/offline-first-app/app/index.tsx
@@ -1,15 +1,41 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, View, RefreshControl } from "react-native";
+import { loadCachedData } from "./utils/storage";
+import { fetchAndCacheData } from "./utils/fetchData";
+import { FlatList } from "react-native";
+import ListItem from "./components/ListItem";
+import Buttons from "./components/Buttons";
 
 export default function Index() {
+  const [content, setContent] = useState<any[]>([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    loadCachedData(setContent);
+  });
+
+  const handleRefresh = async () => {
+    setRefreshing(true);
+    await fetchAndCacheData(setContent);
+    setRefreshing(false);
+  };
+
+  const handleClear = () => setContent([]);
+  const handleReload = () => loadCachedData(setContent);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <FlatList
+        data={content}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem title={item.title} body={item.body} />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
+        }
+      />
+      <Buttons onClear={handleClear} onReload={handleReload} />
     </View>
   );
 }
