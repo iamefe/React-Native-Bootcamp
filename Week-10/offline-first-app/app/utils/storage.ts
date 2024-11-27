3469b44711392c64afd92c9b3b5271d9d722f1f3
Osarugue Enehizena <nehirugue@gmail.com>
2024-11-27 09:34:57 +0100

    done with storage.ts

---
+++ Week-10/offline-first-app/app/utils/storage.ts
@@ -0,0 +1,16 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export async function loadCachedData(setContent: (data: any[]) => void) {
+  try {
+    const cachedData = await AsyncStorage.getItem("cachedContent");
+    if (cachedData) {
+      setContent(JSON.parse(cachedData));
+    }
+  } catch (error) {
+    console.error("Error loading cached data: ", error);
+  }
+}
+
+export async function clearCachedContent() {
+  await AsyncStorage.removeItem("cachedContent");
+}
