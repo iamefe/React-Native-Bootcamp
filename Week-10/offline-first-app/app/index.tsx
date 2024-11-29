bc7868d3945baf69925c8ef0204d77c85d5ddcbd
Osarugue Enehizena <nehirugue@gmail.com>
2024-11-29 08:36:11 +0100

    done with refined project

--- Week-10/offline-first-app/app/index.tsx
+++ Week-10/offline-first-app/app/index.tsx
@@ -1,20 +1,29 @@
 import { useEffect, useState } from "react";
-import { View, RefreshControl, StyleSheet } from "react-native";
+import { View, RefreshControl, StyleSheet, Alert, Text } from "react-native";
 import { loadCachedData } from "../utils/storage";
 import { fetchAndCacheData } from "../utils/fetchData";
 import { FlatList } from "react-native";
 import ListItem from "../components/ListItem";
 import Buttons from "../components/Buttons";
+import useNetworkStatus from "@/utils/useNetworkStatus";
 
 export default function Index() {
   const [content, setContent] = useState<any[]>([]);
   const [refreshing, setRefreshing] = useState(false);
+  const isConnected = useNetworkStatus();
 
   useEffect(() => {
     loadCachedData(setContent);
   }, []);
 
   const handleRefresh = async () => {
+    if (!isConnected) {
+      Alert.alert(
+        "Offline",
+        "You are currently offline. Data cannot be refreshed."
+      );
+      return;
+    }
     setRefreshing(true);
     await fetchAndCacheData(setContent);
     setRefreshing(false);
@@ -25,6 +34,11 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
+      {!isConnected && (
+        <Text style={styles.offlineBanner}>
+          You're offline. Displaying cached data
+        </Text>
+      )}
       <FlatList
         data={content}
         keyExtractor={(item) => item.id.toString()}
@@ -45,5 +59,13 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     backgroundColor: "#f5f5f5",
+  },
+  offlineBanner: {
+    textAlign: "center",
+    padding: 10,
+    backgroundColor: "#ffd700",
+    color: "#000",
+    fontWeight: "bold",
+    marginBottom: 10,
   },
 });
